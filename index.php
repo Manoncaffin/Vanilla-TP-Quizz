9773dc1446f73b37146d2bb25d4b5ed3e8f44a7f
Caffin <caffinmanon@gmail.com>
2023-12-20 17:28:22 +0100

    page index en cours

---
+++ index.php
@@ -0,0 +1,30 @@
+<?php
+include_once('./partials/header.php');
+?>
+
+
+<main>
+    <section id="one" class="d-flex justify-content-center">
+        <div class="container-md col-5 text-center">
+            <h2>Bienvenue sur le quizz !</h2>
+
+            <form action="./process/user_verif_pseudo.php" method="post">
+                <div>
+                    <label for="pseudo" class="form-label pt-5 pb-2">Entrez votre pseudo pour jouer</label>
+                    <input type="text" class="form-control" id="pseudo" name="pseudo" required />
+                    <div class="pt-2">
+                        <button type="submit" class="btn btn-light btn-lg text-black">VALIDER</button>
+                    </div>
+                </div>
+        </div>
+    </section>
+</main>
+
+<?php
+// header('Location: ./pages/play.php');
+?>
+
+
+<?php
+include_once('./partials/footer.php');
+?>
