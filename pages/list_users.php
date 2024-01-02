ce4758289b7b73e5fb30ae37370199ebf50f40ec
Caffin <caffinmanon@gmail.com>
2024-01-02 14:54:28 +0100

    suppression dossier partials

--- pages/list_users.php
+++ pages/list_users.php
@@ -1,7 +1,14 @@
-<?php
-include_once('../partials/header.php');
-?>
-
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Bienvenue sur le quizz !</title>
+    <link rel="stylesheet" href="/Exercices_PHP/Vanilla-TP-Quizz/style.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <link href="https://fonts.googleapis.com/css2?family=Capriola&family=Poppins:wght@500&display=swap" rel="stylesheet">
+</head>
+<body>
 
 <main>
     <section id="four">
@@ -32,6 +39,6 @@ include_once('../partials/header.php');
     </section>
 </main>
 
-<?php
-include_once('../partials/footer.php');
-?>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+</body>
+</html>
--- pages/play.php
+++ pages/play.php
@@ -1,7 +1,18 @@
 <?php
-include_once('../partials/header.php');
 session_start();
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Bienvenue sur le quizz !</title>
+    <link rel="stylesheet" href="/Exercices_PHP/Vanilla-TP-Quizz/style.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <link href="https://fonts.googleapis.com/css2?family=Capriola&family=Poppins:wght@500&display=swap" rel="stylesheet">
+</head>
+<body>
 
 
 <h2>Bonjour <?php echo $_SESSION['pseudo'] ?></h2>
@@ -9,6 +20,6 @@ session_start();
 
 
 
-<?php
-include_once('../partials/footer.php');
-?>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+</body>
+</html>
